7f084b46e1b746c02e28a8bcc7ff0a1c6626178f
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-05-08 01:11:52 +0900

    storiesディレクトリを移動

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import { HomeScreen, DetailsScreen } from "../src/screens";
+import { HomeScreen, DetailsScreen } from "../screens";
 
 storiesOf("screens", module)
   .add("HomeScreen", () => <HomeScreen />)
